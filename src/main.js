57eaa29d9f478f82ebc27a4740cc094937963be4
fireworks80 <fireworks80@naver.com>
2020-03-07 12:22:29 +0900

    vue router 예시

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const Login = { template: '<div>Login</div>' };
+
+//  routing map
+const routes = {
+  '/': App,
+  '/login': Login
+};
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  computed: {
+    VueComponent() {
+      // 경로에 따라 화면을 다르게 그린다.
+      return (
+        routes[location.pathname] || {
+          template: '<div>404 not found</div>'
+        }
+      );
+    }
+  },
+  render(h) {
+    // 화면을 그린다.
+    return h(this.VueComponent);
+  }
+}).$mount('#app');
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+//  runtimeCompile 이 기본 false
+// 로 되어있어서 main.js에서 템플릿을 만들어쓰면 오류가 생김
+module.exports = {
+  runtimeCompiler: true
+};
