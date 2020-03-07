4a2d4365655024a6b047cb557864c92020c678c7
fireworks80 <fireworks80@naver.com>
2020-03-07 20:11:29 +0900

    vue-router이용한 routing 작업완료

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const Login = { template: '<div>Login page</div>' };
+const NotFound = { template: '<div>Not found</div>' };
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: App },
+    { path: '/login', component: Login },
+    { path: '*', component: NotFound }
+  ]
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h({ template: '<router-view></router-view>' })
+}).$mount('#app');
