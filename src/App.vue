46ff1b775f979e7892af358932375e2d27acb8dc
fireworks80 <fireworks80@naver.com>
2020-03-10 08:28:21 +0900

    Navbar router link 컴포넌트 생성

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
     <h1>Welcome to Your Vue.js App</h1>
+    <Navbar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
+import Navbar from './components/Navbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: { Navbar }
 }
 </script>
 
