e7ce2e863ed7bceb4ad135e4138d1af290471e73
fireworks80 <fireworks80@naver.com>
2020-03-12 07:43:21 +0900

    데이터 불러오기 예시

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <h1>Welcome</h1>
+    
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from './components/Navbar'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Navbar}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    home
+    <dl>
+      <dt>Board List</dt>
+      <dd>
+        <ul>
+          <li><router-link to="/b/1">Board 1</router-link></li>
+          <li><router-link to="/b/2">Board 2</router-link></li>
+        </ul>
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
