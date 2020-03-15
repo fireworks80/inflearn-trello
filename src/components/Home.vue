48084622684871804ef9c1c49763de1fdc7f5833
fireworks80 <fireworks80@naver.com>
2020-03-16 08:13:11 +0900

    XMLHttpRequest를 이용한 api 호출

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,10 @@
   <div>
     home
     <dl>
-      <dt>Board List</dt>
+      <dt>Board List:</dt>
       <dd>
+        <p v-if="loading">loading.....</p>
+        <p v-else>API result: {{apiRes}}</p>
         <ul>
           <li><router-link to="/b/1">Board 1</router-link></li>
           <li><router-link to="/b/2">Board 2</router-link></li>
@@ -15,7 +17,34 @@
 
 <script>
   export default {
-    name: 'Home'
+    name: 'Home',
+    data() {
+      return {
+        loading: false,
+        apiRes: ''
+      }
+    },
+    created() {
+      this.fetchData();
+    },
+    methods: {
+      fetchData() {
+        this.loading = true;
+
+        const req = new XMLHttpRequest();
+
+        req.open('GET', 'http://localhost:3000/health');
+        req.send();
+        req.addEventListener('load', () => {
+          this.loading = false;
+          this.apiRes = {
+            status: req.status,
+            statusText: req.statusText,
+            response: JSON.parse(req.response)
+          }
+        });
+      }
+    }
   }
 </script>
 
