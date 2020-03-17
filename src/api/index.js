dd3716f85b3e99913538c97a2951a5e8fc9bd3c5
fireworks80 <fireworks80@naver.com>
2020-03-18 08:32:57 +0900

    login 구현

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ import router from '../router';
 const DOMAIN = 'http://localhost:3000';
 const UNAUTHORIZED = 401;
 const onUnauthorized = () => {
-  router.push('/login');
+  router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`);
 };
 
 const request = (method, url, data) => {
@@ -20,12 +20,25 @@ const request = (method, url, data) => {
       } = res.response;
       if (status === UNAUTHORIZED) return onUnauthorized();
 
-      new Error(res);
+      throw res.response;
     });
+};
+
+export const setAuthInHeader = token => {
+  axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
 };
 
 export const board = {
   fetch() {
     return request('GET', '/boards');
+  }
+};
+
+export const auth = {
+  login(email, password) {
+    return request('post', '/login', {
+      email,
+      password
+    });
   }
 };
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,59 @@
 <template>
-  <div>
-    Login
+  <div class="login">
+    <h2>Log in to Trello</h2>
+    <form @submit.prevent="onSubmit">
+      <p><label for="email">Email: </label><input class="form-control" v-model="email" id="email" type="email" playceholder="test@test.com"></p>
+      <p><label for="pw">Password: </label><input class="form-control" v-model="password" id="pw" type="password" playceholder="123123"></p>
+      <button class="btn" :class="{'btn-success': !invalidForm}" :disabled="invalidForm">log in</button>
+    </form>
+    <p class="error" v-if="error">login Failer</p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Login'
+import { auth, setAuthInHeader } from '../api';
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: '',
+      rPath: ''
+    };
+  },
+  computed: {
+    invalidForm() {
+      return !this.email || !this.password;
+    }
+  },
+  created() {
+    this.rPath = this.$route.query.rPath || '/';
+  },
+  methods: {
+    onSubmit() {
+      auth.login(this.email, this.password)
+          .then(res => {
+            localStorage.setItem('token', res.accessToken);
+            setAuthInHeader(res.accessToken);
+            this.$router.push(this.rPath);
+          })
+          .catch( error => {
+            this.error = error;
+            console.log(error);
+          })
+    }
   }
+}
 </script>
 
-<style lang="scss" scoped>
+<style>
+.login {
+  width: 400px;
+  margin: 0 auto; 
+}
+.error {
+  color: #f00;
+}
 
 </style>
