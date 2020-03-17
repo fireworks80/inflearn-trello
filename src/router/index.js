d672287d83c5c8d19ee5815b91870cbd53fa3ea2
fireworks80 <fireworks80@naver.com>
2020-03-18 07:05:56 +0900

    navigation guard 추가

--- src/router/index.js
+++ src/router/index.js
@@ -8,17 +8,42 @@ import NotFound from '../components/NotFound';
 
 Vue.use(VueRouter);
 
+// (401에러가 떠서 토큰이 없다는 것을 알고 로그인페이지로 가지 않고)
+// api를 호출하기 전에 로그인 여부를 확인 하여 토큰을 발급 받지 않았을 경우 
+// 로그인페이지로 이동
+
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem('token');
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`;
+  isAuth ? next() : next(loginPath);
+};
+
+
 const router = new VueRouter({
   mode: 'history',
-  routes: [
-    { path: '/', component: Home },
-    { path: '/login', component: Login },
+  routes: [{
+      path: '/',
+      component: Home,
+      beforeEnter: requireAuth
+    },
+    {
+      path: '/login',
+      component: Login
+    },
     {
       path: '/b/:bid',
       component: Board,
-      children: [{ path: 'c/:cid', component: Card }]
+      beforeEnter: requireAuth,
+      children: [{
+        path: 'c/:cid',
+        component: Card,
+        beforeEnter: requireAuth
+      }]
     },
-    { path: '*', component: NotFound }
+    {
+      path: '*',
+      component: NotFound
+    }
   ]
 });
 
